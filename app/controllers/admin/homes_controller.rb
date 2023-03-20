7ef58db80eda8667df4fc03de579c31ad7d9c651
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-03-20 06:52:42 +0000

    in the middle

---
+++ app/controllers/admin/homes_controller.rb
@@ -0,0 +1,7 @@
+class Admin::HomesController < ApplicationController
+
+def top
+    @customer=Customer.all
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
   # 管理者用
   # URL /admin/sign_in ...
-  devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "customer/registrations",
-  sessions: 'customer/sessions'
-}
-  # 顧客用
-  # URL /customers/sign_in ...
+
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+namespace :admin do
+  resources :customers, only:[:index, :show, :edit, :update]
+end
+get 'admins' => 'admin/homes#top'
+
+
+  # 顧客用
+  # URL /customers/sign_in ...
+ devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
+}
+ root to: 'customer/homes#top'
+ get 'customers/my_page/:id' => 'customer/customers#show'
+ get 'customers/information/edit' => 'customer/customers#edit'
+ get 'customers/information' => 'customer/customers#update'
+ get 'customers/quit' => 'customer/customers#quit'
+ get 'customers/out' => 'customer/customers#out'
+
 end
