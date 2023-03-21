8eaf1f1e5039990c6196e81edd76ffe2b5d98384
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2023-03-21 08:44:47 +0000

    ジャンル・配送先調整

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,12 +24,5 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  protected
-  def after_sign_in_path_for(resource)
-    admin_items_path
-  end
 
-  def after_sign_out_path_for(resource)
-    new_admin_item_path_session_path
-  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,24 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # def after_sign_in_path_for(resource)
+  #   case resource
+  #   when Admin
+  #     admin_items_path
+  #   when Customer
+  #     root_path
+  #   end
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   case resource
+  #   when :admin
+  #     new_admin_session_path
+  #   when :customer
+  #     root_path
+  #   end
+  # end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
