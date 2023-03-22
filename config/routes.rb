662e43d5f35f2e4e08c7df5084572533a095c71d
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-03-22 04:28:17 +0000

    途中

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 }
   # 管理者用
   # URL /admin/sign_in ...
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
   namespace :admin do
