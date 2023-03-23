0ee177ea294c6db7cce4f10df33a7117d715e68d
DMM-Camper-to4ya <to4ya.oockv36oo@gmail.com>
2023-03-23 11:03:33 +0000

    レイアウト整える

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,10 @@
 class Customer < ApplicationRecord
+  # is_deletedがfalseならtrueを返すようにしている
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :admin
 end
