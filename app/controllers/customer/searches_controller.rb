053402af15723611ba6a88555e516a0564aa7ff4
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-03-25 08:46:40 +0000

    更新

--- app/controllers/customer/searches_controller.rb
+++ app/controllers/customer/searches_controller.rb
@@ -4,7 +4,7 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
 
-    if @range == "User"
+    if @range == "Item"
       @users = User.looks(params[:search], params[:word])
     else
       @books = Book.looks(params[:search], params[:word])
