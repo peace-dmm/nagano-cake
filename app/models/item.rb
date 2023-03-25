db4b0a3a58f2f97dcdec09570836cb38e9418467
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-03-25 04:08:47 +0000

    カートitem修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
-  
+
   has_many :cart_item
   has_many :order_detail
   has_many :order, through: :order_details
   belongs_to :genre#, optional: true#実装されたら外す
 
-  validates :amount, presence: true
+  #validates :amount, presence: true
   validates :genre_id, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
