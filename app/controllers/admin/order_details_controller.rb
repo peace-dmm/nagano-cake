887df53de954f8349c6283857a490f58c510a205
EC2 Default User <ec2-user@ip-172-31-30-118.ap-northeast-1.compute.internal>
2023-03-25 07:21:32 +0000

    注文機能

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,9 +4,9 @@ class Admin::OrderDetailsController < ApplicationController
     def update
     order_details = OrderDetails.find(params[:id])
     is_updated = true
-    if @order_detail.update(order_detail_params)
-      @order.update(status: 2) if @order_detail.making_status == "in_production"
-      @order_details.each do |order_detail|
+    if order_detail.update(order_detail_params)
+      order.update(status: 2) if order_detail.making_status == "in_production"
+      order_details.each do |order_detail|
       if order_detail.making_status != "production_complete"
       is_updated = false
       end
