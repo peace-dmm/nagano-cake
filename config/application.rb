c113a0298952423b774d97e60ce878f6cfbe5851
EC2 Default User <ec2-user@ip-172-31-30-118.ap-northeast-1.compute.internal>
2023-03-25 13:01:27 +0000

    注文機能

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,10 @@ module NaganoCake
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
-
+    config.time_zone = 'Asia/Tokyo'
     # Configuration for the application, engines, and railties goes here.
-    #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    config.time_zone = 'Asia/Tokyo'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
