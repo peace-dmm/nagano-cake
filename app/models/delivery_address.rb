28c8ce8c764b0a76ae89c27aa1f65c8cf9a1df0e
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2023-03-26 05:58:07 +0000

    配送先調整

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -3,4 +3,8 @@ class DeliveryAddress < ApplicationRecord
   def address_display
   '〒' + postal_code + ' ' + address + ' ' + name
   end
+
+  validates :address, presence: true
+  validates :postal_code, presence: true
+  validates :name, presence: true
 end
